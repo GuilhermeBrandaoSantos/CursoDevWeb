481862d18ca57acaf8e8f8639cc659dc34852cb8
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2017-10-24 22:11:22 -0200

    Add icons

--- ProjetoFinal-TwitterClone/view/home.php
+++ ProjetoFinal-TwitterClone/view/home.php
@@ -14,7 +14,23 @@
 		<!-- jquery - link cdn -->
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 		<!-- Icon -->
-    <link rel="icon" sizes="192x192" href="../imagens/icone_twitter.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="../imagens/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="../imagens/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="../imagens/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="../imagens/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="../imagens/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="../imagens/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="../imagens/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="../imagens/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="../imagens/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="../imagens/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="../imagens/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="../imagens/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="../imagens/favicon-16x16.png">
+		<link rel="manifest" href="../imagens/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="../imagens/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
 		<!-- bootstrap - link cdn -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">	
 		
